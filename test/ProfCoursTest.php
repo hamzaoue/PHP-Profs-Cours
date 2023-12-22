aef70183c2d90410254091832be8115080661549
Erwan <erwan.hamzaoui@edu.esiee.fr>
2023-12-22 03:24:56 +0100

    test

--- test/ProfCoursTest.php
+++ test/ProfCoursTest.php
@@ -220,7 +220,7 @@ class ProfCoursTest extends TestCase
         * s’inspirer de test de la sélection et affichage des profs pour tester la sélection et l’affichage des cours.   
         *
         */
-        $recordCourseArray = Course::printAll($conn);
+        $recordCourseArray = Cours::printAll($conn);
         print "+++++++++++++++++++++ - LISTE DES COURS ET LEUR PROF - ++++++++++++++++++++\n";
         foreach ($recordCourseArray as $recordCourse) {
             print $recordCourse;
